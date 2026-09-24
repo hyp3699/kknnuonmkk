0fd955a5cdf68e3a69065902980faf19629488ff
hhhpojj <heyuping369@outlook.com>
2026-09-24 12:10:41 +0800

    Fix formatting issue in sing-box-cf08.sh

--- jiao/sing-box-cf08.sh
+++ jiao/sing-box-cf08.sh
@@ -11648,7 +11648,7 @@ menu() {
    printf "%b%-32s%b%s%b\n" "$green" "5. 查看节点信息" "$red" "14. 本机信息" "$re"
    printf "%b%-32s%b%s%b\n" "$green" "6. 配置文件查看" "$red" "15. WARP分流管理" "$re"
    printf "%b%-32s%b%s%b\n" "$green" "7. 管理节点订阅" "$red" "16. token"    "$re"
-   printf "%b%-28s%b%s%b\n" "$green" "8. 更新sing-box""$red" "17. vps"      "$re"
+   printf "%b%-28s%b%s%b\n" "$green" "8. 更新sing-box" "$red" "17. vps"     "$re"
    printf "%b%-32s%b%s%b\n" "$green" "9. 添加删除节点"                     "$re"
    echo
    printf "%b%-32s%b%s%b\n" "$green" "99. 查看错误信息" "$red" "0. 退出脚步" "$re"
