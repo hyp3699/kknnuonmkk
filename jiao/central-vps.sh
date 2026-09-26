15bac87e06676b1b747598b291fa41c450c321ef
hhhpojj <heyuping369@outlook.com>
2026-09-26 11:35:43 +0800

    Update central-vps.sh

--- jiao/central-vps.sh
+++ jiao/central-vps.sh
@@ -1218,7 +1218,7 @@ open_vps_menu() {
         -o ConnectTimeout=8 \
         -o ServerAliveInterval=15 \
         -o ServerAliveCountMax=3 \
-        root@"$address" sb
+        root@"$address" sb || true
     echo
     green "已退出 $name 管理菜单"
     read -rp "按 Enter 返回..." _
@@ -1253,7 +1253,7 @@ open_vps_ssh() {
         -o ConnectTimeout=8 \
         -o ServerAliveInterval=15 \
         -o ServerAliveCountMax=3 \
-        root@"$address" 
+        root@"$address"  || true
     echo
     green "已退出 $name 管理菜单"
     read -rp "按 Enter 返回..." _
